7e1378d3448377786cd25588f9edc46cea25e5f5
Don-001 <claudiodex009@gmail.com>
2023-07-13 21:30:57 +0300

    create models for the database tables

--- app/models.py
+++ app/models.py
@@ -2,7 +2,6 @@ from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
-
 from .database import Base
 
 
